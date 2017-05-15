4d389a2fca34c55fafc74dad4e7a6283da2ccc72
Zyigh <hugo.medina@hetic.net>
2017-05-15 11:34:12 +0200

    c'est pas bien de forcer...

--- index.php
+++ index.php
@@ -6,21 +6,3 @@ require_once "vendor/autoload.php";
 
 $build = new Builder('articles', Setup::getConnexion());
 
-echo "<pre>";
-
-$insert = array('titre' => 'Ulysse téma',
-				'auteur' => 'Zyigh',
-				'commentaire' => 'normalement ça marche...');
-
-
-
-// var_dump($build->delete(14)->get());
-// $datas = $build->select('id')->get();
-
-// $build->insert($insert)->get();
-$datas = $build->select('titre')->select('auteur', 'commentaire')->where(12)->get();
-
-
-var_dump($datas);
-
-echo "<br>";
