73bdd8c4c9f3fe2f756fd176a8b1c36980ba8665
UlysseHall <ulysse.hall@hetic.net>
2017-05-16 18:24:26 +0200

    envoie des 4 dernières news à home.php

--- app/core/Setup.php
+++ app/core/Setup.php
@@ -8,8 +8,8 @@ class Setup
 	CONST DB_NAME = 'architecture';
 	CONST DB_HOST = 'localhost';
 	CONST DB_USER = 'root';
-	CONST DB_PASS = 'root';
-	CONST DB_PORT = '8888';
+	CONST DB_PASS = 'kamini';
+	CONST DB_PORT = '3306';
 
 	/**
 	 * @var \PDO
