d0099e1bf524ae7f5af1558f7b013ed9ee4d2409
lacaux-gerome <gerome.lacaux@hetic.net>
2017-05-16 22:58:09 +0200

    listNews/tender/home responsive

---
+++ pages/tender.php
@@ -0,0 +1,16 @@
+<div class="banner backgroundTender">
+	<h1>Appel d’offre <br>
+		Ne ratez plus aucun appel d’offres</h1>
+</div>
+
+<div class="aboutTender">
+	<p>
+		DoubleTrade vous accompagne depuis 1998 sur l’ensemble du processus  de veille et de gestion commerciale sur les marchés publics et projets privés.
+	</p>
+</div>
+<div class="containerMentionTender">
+	<button class="buttonTender">
+		<img src="../public/images/img-layout/right-arrow.png" alt=""><a href="">Consultez</a>
+	</button>
+</div>
+	<p class="mentionTender">DoubleTrade conserve plus de 12 584 582 annonces dans sa base historique.</p>
