836ea2a6ca9fa1d5eb59d99a0ed73ecc8db1bb1b
UlysseHall <ulysse.hall@hetic.net>
2017-05-17 12:07:02 +0200

    màj bdd

--- app/architecture.sql
+++ app/architecture.sql
@@ -1,29 +1,24 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.2.11
+-- http://www.phpmyadmin.net
 --
--- Client :  localhost:8889
--- Généré le :  Lun 15 Mai 2017 à 16:34
--- Version du serveur :  5.6.35
--- Version de PHP :  7.1.1
+-- Client :  127.0.0.1
+-- GÃ©nÃ©rÃ© le :  Mer 17 Mai 2017 Ã  12:04
+-- Version du serveur :  5.6.21
+-- Version de PHP :  5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
---
--- Base de données :  `architecture`
---
 
--- --------------------------------------------------------
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Structure de la table `category`
+-- Base de donnÃ©es :  `architecture`
 --
-
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `type` text COLLATE utf8mb4_unicode_ci
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -31,14 +26,14 @@ CREATE TABLE `category` (
 -- Structure de la table `magazine`
 --
 
-CREATE TABLE `magazine` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `magazine` (
+`id` int(11) NOT NULL,
   `number` int(11) DEFAULT NULL,
   `img` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `pdf` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `region` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `year` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -46,8 +41,8 @@ CREATE TABLE `magazine` (
 -- Structure de la table `news`
 --
 
-CREATE TABLE `news` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `news` (
+`id` int(11) NOT NULL,
   `date` date DEFAULT NULL,
   `architect` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `department` int(11) DEFAULT NULL,
@@ -56,72 +51,40 @@ CREATE TABLE `news` (
   `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `description` text COLLATE utf8mb4_unicode_ci,
   `infos` text COLLATE utf8mb4_unicode_ci,
-  `partners` text COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `partners`
---
-
-CREATE TABLE `partners` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `location` int(11) NOT NULL,
-  `magazine_id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `partners` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `img` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Index pour les tables exportées
+-- Index pour les tables exportÃ©es
 --
-
---
--- Index pour la table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `magazine`
 --
 ALTER TABLE `magazine`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `news`
 --
 ALTER TABLE `news`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `partners`
---
-ALTER TABLE `partners`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
+-- AUTO_INCREMENT pour les tables exportÃ©es
 --
 
---
--- AUTO_INCREMENT pour la table `category`
---
-ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `magazine`
 --
 ALTER TABLE `magazine`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT pour la table `news`
 --
 ALTER TABLE `news`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `partners`
---
-ALTER TABLE `partners`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
