dcd81502b709e8126e2b373a6c284ab7e1e24b8d
Zyigh <hugo.medina@hetic.net>
2017-05-17 23:14:31 +0200

    header admin dans le template

--- template/default.php
+++ template/default.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
 	<title>Larchitecture</title>
 	<link rel="stylesheet" href="public/css/style.css">
-	<link rel="stylesheet" href="public/css/screen.css">
 
 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
@@ -12,9 +11,7 @@
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
-    <?php
-        if($controllerName == "Admin") {
-    ?>
+
     <!-- Bootstrap core CSS     -->
     <link href="pages/admin/assets/css/bootstrap.min.css" rel="stylesheet" />
 
@@ -33,11 +30,10 @@
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
     <link href="pages/admin/assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
-    <?php } ?>
 </head>
 <body>
     <?php
-        if($controllerName != "Admin") {
+        if($controllerName != "Admin") :
     ?>
     <input type="checkbox" id="check1" name=""/>
 	<nav class="navPhone">
@@ -62,19 +58,65 @@
 	</nav>
 	<header>
 		<ul>
-			<li><a href="index.php"><img class="logo" src="public/images/img-layout/logo_larchitecture_black.png" alt=""></a></li>
-			<li><a href="index.php?action=listJournals">magazines</a></li>
-			<li><a href="index.php?action=tender">appel d'offre</a></li>
-			<li><a href="index.php?action=listNews">atualités</a></li>
-			<li><a href="index.php?action=order">abonnez-vous</a></li>
+			<li><a href=""><img class="logo" src="public/images/img-layout/logo_larchitecture_black.png" alt=""></a></li>
+			<li><a href="">magasines</a></li>
+			<li><a href="">appel d'offre</a></li>
+			<li><a href="">atualités</a></li>
+			<li><a href="">contact</a></li>
+			<li><a href="">abonnez-vous</a></li>
 			<li><a href=""><img class="logoLang" src="public/images/img-layout/france.png" alt=""> fr <img src="public/images/img-layout/cross_bottom.png" alt=""></a></li>
 		</ul>
 	</header>
-	<?php } ?>
+	<?php else :
+
+	 ?>
+	<div class="wrapper">
+	    <div class="sidebar" data-color="azure" data-image="assets/img/sidebar-5.jpg">
+	        <div class="sidebar-wrapper">
+	            <div class="logo">
+	                <a href="index.php?action=admin_home" class="simple-text">
+	                    Tableau de bord
+	                </a>
+	            </div>
+	            <ul class="nav">
+	                <li class="active">
+	                    <a href="index.php?action=admin_home">
+	                        <i class="pe-7s-home"></i>
+	                        <p>Tableau de bord</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="index.php?action=admin_form&add=1">
+	                        <i class="pe-7s-note"></i>
+	                        <p>Ajouter</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="<?= isset($_GET['id']) ? 'index.php?action=admin_form&update=1&id='.$_GET['id'] : "index.php?action=admin_home" ?>">
+	                        <i class="pe-7s-tools"></i>
+	                        <p>Modifier</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="<?= isset($_GET['id']) ? 'index.php?action=admin_single&id='.$_GET['id'] : "index.php?action=admin_home" ?>">
+	                        <i class="pe-7s-trash"></i>
+	                        <p>Supprimer</p>
+	                    </a>
+	                </li>
+	                <li class="active-pro">
+	                    <a href="upgrade.html">
+	                        <i class="pe-7s-right-arrow"></i>
+	                        <p>Aller sur le site</p>
+	                    </a>
+	                </li>
+	            </ul>
+	        </div>
+	    </div>
+	<?php endif; ?>
 
 <?php require("pages/" . $data["page"]); ?>
 
-    <footer <?php if($action == "orderAction") { ?> class="footer-stick" <?php } ?>>
+    <footer>
 		<div class="containerFooter">
 			<div class="containerLeft">
 				<h2>Contact</h2>
