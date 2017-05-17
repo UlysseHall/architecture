f9ab03e55fc2938d07fc71608d7359ac343f1865
IamSamDiak <samuel.diakumpuna@hetic.net>
2017-05-17 16:56:39 +0200

    page validation

---
+++ pages/validation.php
@@ -0,0 +1,27 @@
+<div id="commande-confirmee">
+    <div class="commande-confirmee-background-image-width">
+        <h3>Merci de votre commande</h3>
+    </div>
+</div>
+<div id="merci-commande">
+    <div class="merci-commande-width">
+        <div>
+            <div class="monsieur-ravelli">
+                <p>Bonjour M. Ravelli,</p>
+            </div>
+            <div class="confirmer-commande">
+                <p>Nous avons le plaisir de vous confirmer votre commande N°2017C1450099630I3.</p>
+            </div>
+            <div class="desktop-image">
+                <figure>
+                    <img src="images/mail.png">
+                </figure>
+            </div>
+            <div class="confirmer-commande-suite">
+                <p>Dans l'attente, n'hésitez pas à nous contacter pour toute demande complémentaire.</p>
+                <p>Bien cordialement.
+                    <br>Votre Service Client L’Architecture de votre région.</p>
+            </div>
+        </div>
+    </div>
+</div>
