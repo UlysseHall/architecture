5c5fa924b8c98b0761acc80a2311d7eaee7baeb1
Zyigh <hugo.medina@hetic.net>
2017-05-17 18:56:00 +0200

    Fanny elle est chanmé aussi en vrai

--- pages/admin/form.php
+++ pages/admin/form.php
@@ -1,3 +1,6 @@
+<?php
+$magazine = isset($data["cont"]["magazine"]) ? $data["cont"]["magazine"] : false;
+?>
 <div class="wrapper">
     <div class="sidebar" data-color="azure" data-image="assets/img/sidebar-5.jpg">
 
@@ -131,74 +134,52 @@
                             </div>
                             <div class="content">
                             <!-- 8 -->
-<form method="post" action="index.php?action=admin_form&add=1">
-    <div class="content">
-            <div class="row">
-                <div class="col-md-5">
-                    <div class="form-group">
-                        <label>Nom de votre magazine</label>
-                        <input type="text" class="form-control" placeholder="">
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="form-group">
-                        <label>Numéro</label>
-                        <input type="number" class="form-control" name="number" placeholder="286">
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Année</label>
-                        <input type="number" class="form-control" name="year" placeholder="Année de votre magazine">
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label>Région</label>
-                        <input type="text" class="form-control" name="region" placeholder="Votre région FR" >
-                    </div>
-                </div>                                    
-            </div>
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <label>Couverture</label>
-                        <input type="text" class="form-control" name="img" placeholder="Insérer votre couverture">
-                        <input type="file" placeholder="Couverture" >
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <label>PDF</label>
-                        <input type="text" class="form-control" name="pdf" placeholder="Insérer votre PDF">
-                        <input type="file" placeholder="test" >
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <label>Votre article</label>
-                        <textarea rows="5" class="form-control" placeholder="" value="Mike">Depuis 1990, la revue "L'Architecture de votre région" propose un panorama détaillé de l'architecture d'une région, d'un territoire ou d'un pays.</textarea>
-                    </div>
-                </div>
-            </div>
-
-            <input type="hidden" name="add" value="1">
-
-            <button type="submit" class="btn btn-info btn-fill pull-right">Ajouter votre magazine</button>
-            <div class="clearfix"></div>
+                                <form method="post" action="index.php?action=admin_form&add=1">
+                                    <div class="content">
+                                            <div class="row">
+                                                <div class="col-md-5">
+                                                    <div class="form-group">
+                                                        <label>Région</label>
+                                                        <input type="text" class="form-control" name="region" <?=  $magazine ? 'value="'.$magazine->getRegion().'"' : 'placeholder="Votre région FR"' ?>>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-3">
+                                                        <div class="form-group">
+                                                            <label>Numéro</label>
+                                                            <input type="number" class="form-control" name="number" <?= $magazine ? 'value="'.$magazine->getNumber().'"' : 'placeholder="Numéro"' ?>>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <div class="form-group">
+                                                            <label for="exampleInputEmail1">Année</label>
+                                                            <input type="number" class="form-control" name="year" <?= $magazine ? 'value="'.$magazine->getYear().'"' : 'placeholder="Année de votre magazine"' ?>>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-md-12">
+                                                        <div class="form-group">
+                                                            <label>Couverture</label>
+                                                            <input type="text" class="form-control" name="img" <?= $magazine ? 'value="'.$magazine->getImg().'"' : 'placeholder="Insérer votre couverture"' ?>>
+                                                            <input type="file" placeholder="Couverture">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-md-12">
+                                                        <div class="form-group">
+                                                            <label>PDF</label>
+                                                            <input type="text" class="form-control" name="pdf" <?= $magazine ? 'value="'.$magazine->getPdf().'"' : 'placeholder="Insérer votre PDF"' ?>>
+                                                            <input type="file" placeholder="test" >
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <input type="hidden" name="add" value="1">
+                                                <button type="submit" class="btn btn-info btn-fill pull-right">Ajouter votre magazine</button>
+                                                <div class="clearfix"></div>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
                                 </form>
                             </div>
                         </div>
@@ -220,14 +201,11 @@
                                 </div>
                             </div>
                             <hr>
-
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
-
     </div>
 </div>
 
