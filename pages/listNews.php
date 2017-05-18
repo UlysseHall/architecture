df41c6644d251391be7654085e1be509a3074674
Zyigh <hugo.medina@hetic.net>
2017-05-18 15:31:40 +0200

    yop ?

--- pages/listNews.php
+++ pages/listNews.php
@@ -5,13 +5,13 @@
 
 
 <?php
-foreach($data["cont"]["news"] as $news){ ?>
+foreach($data["cont"]["news"] as $news) : ?>
 	<div class="containerListActu">
 	<a href="">
 		<h4>Date : 	<?php echo $news->getDate(); ?></h4>
 		<h2><?php echo $news->getArchitect(); ?><div class="underline"></div></h2>
 		<h3><?php echo $news->getTitle(); ?> <br>
-<?php echo $news->getCity(); ?></h3>
+	<?php echo $news->getCity(); ?></h3>
 	<div class="containerListActuImg">
 		<img class="mainImg" src="public/images/img-content/<?php echo $news->getImg()[0]; ?>" alt="">
 		<button>
@@ -23,5 +23,5 @@ foreach($data["cont"]["news"] as $news){ ?>
 	</div>
 </a>
 <?php
-}
+endforeach;
 ?>
